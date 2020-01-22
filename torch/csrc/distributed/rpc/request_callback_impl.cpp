134df96dac542cebfec0097bbd929d7a113a51a3
Rohan Varma <rvarm1@fb.com>
2020-01-21 18:08:46 -0800

    [rpc] make handling of FORWARD_AUTOGRAD_REQ in request_callback_impl nonblocking.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32476
    
    This makes the handling of FORWARD_AUTOGRAD_REQ in request_callback
    nonblocking. Processing this message requires unwrapping the message with
    autograd information, processing the original message, and sending back the
    message with autograd information wrapped. This makes the processing the
    original message nonblocking by grabbing a future to it and marking the parent
    future as completed when this one completes.
    ghstack-source-id: 97013167
    
    Differential Revision: [D19509501](https://our.internmc.facebook.com/intern/diff/D19509501/)

--- torch/csrc/distributed/rpc/request_callback_impl.cpp
+++ torch/csrc/distributed/rpc/request_callback_impl.cpp
@@ -212,14 +212,28 @@ std::shared_ptr<FutureMessage> RequestCallbackImpl::processRpc(
 
       // Process the original RPC.
       auto wrappedMessageType = rpcWithAutograd.wrappedMessageType();
-      auto wrappedRpcResponse = processRpc(
+      // Kick off processing for the nested future and get a Future<T> to the
+      // result.
+      auto wrappedRpcResponseFuture = processRpc(
           rpcWithAutograd.wrappedRpc(), wrappedMessageType, messageId);
-      wrappedRpcResponse->waitNoThrow(); // TODO: make async
 
-      return wrap(getMessageWithAutograd(
-          rpcWithAutograd.fromWorkerId(),
-          std::move(*wrappedRpcResponse).moveValue(),
-          MessageType::FORWARD_AUTOGRAD_RESP));
+      // Make an overall future for the wrapped response.
+      auto responseFuture = std::make_shared<rpc::FutureMessage>();
+      auto fromWorkerId = rpcWithAutograd.fromWorkerId();
+      // The original future needs to be marked as completed when the wrapped
+      // one completes, with the autograd context information wrapped.
+      wrappedRpcResponseFuture->addCallback(
+          [responseFuture, messageId, fromWorkerId, wrappedRpcResponseFuture](
+              const Message& m,
+              const c10::optional<utils::FutureError>& error) {
+            // Hack since getMessageWithAutograd requires a Message&&
+            auto msg = m;
+            responseFuture->markCompleted(getMessageWithAutograd(
+                fromWorkerId,
+                static_cast<torch::distributed::rpc::Message&&>(msg),
+                MessageType::FORWARD_AUTOGRAD_RESP));
+          });
+      return responseFuture;
     }
     case MessageType::BACKWARD_AUTOGRAD_REQ: {
       auto& gradientsCall = static_cast<PropagateGradientsReq&>(rpc);
